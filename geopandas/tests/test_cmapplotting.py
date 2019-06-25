e9ead5be282d8af14f67d6c1d30afc175d91b2d9
Lashuk1729 <kushalborkar.11@gmail.com>
2019-06-25 13:21:50 +0530

    Updated tests

--- geopandas/tests/test_cmapplotting.py
+++ geopandas/tests/test_cmapplotting.py
@@ -23,88 +23,90 @@ class TestCMapPlotting:
     def setup_method(self):
         # Intializing the dataframe
         self.N = 10
-        self.points = GeoSeries(Point(i, i) for i in range(self.N))
+        self.points = GeoSeries(Point(i, i) for i in range(self.N)).buffer(0.5)
         values = np.arange(self.N)
-        levels = ['1','1','2','4','5','1','3','4','2','5']
+        levels = ['1','1','2','4','5','1','5','4','3','2']
         self.df = GeoDataFrame({'geometry': self.points, 'values': values, 'levels': levels})
 
-        # Intializing norm
-        cmap = mpl.cm.viridis_r
-        self.norm = mpl.colors.BoundaryNorm([1,3,6,8], cmap.N)
+    def test_customnorm(self):
+        # test color without additional parameters
+        ax = self.df.plot(column='values')
+        actual_color = ax.collections[0].get_facecolors()
+        cmap = ax.collections[0].get_cmap()
+        expected_color = cmap(np.arange(self.N) / (self.N - 1))
+        np.testing.assert_array_equal(actual_color, expected_color)
 
-    def test_min_max(self):
-        # min, max from the axes must be equal to min, max of the dataframe
-        # checking whether both of them are equal
-        ax = self.df.plot(column='values', norm = self.norm)
-        actual_lim = ax.collections[0].get_clim()
-        expected_lim = (self.df['values'].min(), self.df['values'].max())
-        assert actual_lim == expected_lim
-
-        # test with passing different vmin/vmax
-        ax = self.df.plot(column='values', norm = self.norm, vmin=-10, vmax=20)
-        actual_colors = ax.collections[0].get_facecolors()
-        assert np.any(np.equal(actual_colors[0], actual_colors[1]))
-
-    def test_vmin_vmax(self):
-        # vmin is equal to vmax, all points must have the same color
-        # checking whether all the points have same color
+        # test color with norm
         cmap = mpl.cm.Greys
-        ax = self.df.plot(column='values', norm = self.norm, vmin=0, vmax=0)
-        actual_norm = ax.collections[0].norm(list(self.df['values']))
-        actual_color = cmap(actual_norm)
-        np.testing.assert_array_equal(actual_color[0], actual_color[1])
+        norm = mpl.colors.BoundaryNorm([1,3,6,8], cmap.N)
+        ax = self.df.plot(column='values', norm = norm)
+        actual_color = ax.collections[0].get_facecolors()
+        cmap = ax.collections[0].get_cmap()
+        expected_color = cmap(norm(list(self.df['values'])))
+        np.testing.assert_array_equal(actual_color, expected_color)
 
-    def test_norm(self):
-        # norm from the axis and norm from the dataframe must be equal
-        # checking whether the norm from the axis and norm from the dataframe
-        ax = self.df.plot(column='values', norm = self.norm)
-        actual_norm = ax.collections[0].norm(list(self.df['values']))
+    def test_minmax(self):
+        # test colorbar min,max without additional parameters
+        ax = self.df.plot(column='values', legend=True)
+        colorbar_color = ax.get_figure().axes[1].collections[0].get_clim()
+        point_color = ax.collections[0].get_clim()
+        assert colorbar_color == point_color
 
-        expected_norm = self.norm(list(self.df['values']))
-        np.testing.assert_array_equal(actual_norm,expected_norm)
+        # test colorbar min,max with norm
+        cmap = mpl.cm.Greys
+        norm = mpl.colors.BoundaryNorm([1,3,6,8], cmap.N)
+        ax = self.df.plot(column='values', legend=True, norm = norm)
+        colorbar_color = ax.get_figure().axes[1].collections[0].get_clim()
+        point_color = ax.collections[0].get_clim()
+        assert colorbar_color == point_color
 
-    def testdf_min_max(self):
-        # colorbar from the subset of the dataframe must be not_equal
-        # so, 2 subset of dataframe are made
-        # checking whether the colorbars have same color
-        lvl1 = self.df['levels'] == '1'
-        lvl2 = self.df['levels'] == '5'
-
-        ax = self.df.plot(column='values', legend=True, norm = self.norm)
+    def test_subsets(self):
+        # test min, max of subplot and plot without additional parameters
+        ax = self.df.plot(column='values')
         actual_lim = ax.collections[0].get_clim()
-        actual_color = ax.get_figure().axes[1].collections[0].get_facecolors()
+        actual_color = ax.collections[0].get_facecolors()
 
-        ax1 = self.df[lvl1].plot(column='values', legend=True, norm = self.norm)
+        lvl1 = self.df['levels'] == '5'
+        ax1 = self.df[lvl1].plot(column='values')
         actual_lim1 = ax1.collections[0].get_clim()
-        actual_color1 = ax1.get_figure().axes[1].collections[0].get_facecolors()
+        actual_color1 = ax1.collections[0].get_facecolors()
 
-        ax2 = self.df[lvl2].plot(column='values', legend=True, norm = self.norm)
-        actual_lim2 = ax2.collections[0].get_clim()
-        actual_color2 = ax2.get_figure().axes[1].collections[0].get_facecolors()
+        # Limits must not be equal
+        assert actual_lim != actual_lim1
 
-        np.testing.assert_array_equal(actual_color, actual_color1)
-        np.testing.assert_array_equal(actual_color, actual_color2)
-        np.testing.assert_array_equal(actual_color1, actual_color2)
+        # Colors of min and max for both the plot are equal(which is not correct)
+        np.testing.assert_array_equal(actual_color[0],actual_color1[0])
+        np.testing.assert_array_equal(actual_color[-1],actual_color1[-1])
 
-    def testdf_norm(self):
-        # Norm must be equal(with different norm)
-        # checking whether the norms are equal if different norm is initialized
-        cmap = mpl.cm.tab20
-        norm = mpl.colors.BoundaryNorm([0, 4, 8, 12], cmap.N)
-        ax = self.df.plot(column='values', cmap = cmap, norm = norm)
-        actual_norm = ax.collections[0].norm(list(self.df['values']))
-
-        expected_norm = norm(list(self.df['values']))
-        np.testing.assert_array_equal(actual_norm, expected_norm)
-
-    def test_color(self):
-        # checking whether the cmap are equal if different cmap is provided
+        # test min,max for subplots and plot with norm
+        # obtain min, max and actual colors from axis
         cmap = mpl.cm.Greys
-        norm = mpl.colors.BoundaryNorm([0, 4, 8, 12], cmap.N)
-        ax = self.df.plot(column='values', cmap = cmap, norm = norm)
-        actual_norm = ax.collections[0].norm(list(self.df['values']))
-        actual_color = cmap(actual_norm)
+        norm = mpl.colors.BoundaryNorm([1,3,6,7,9], cmap.N)
+        ax = self.df.plot(column='values',  legend=True, norm = norm)
+        actual_lim = ax.collections[0].get_clim()
+        actual_color = ax.collections[0].get_facecolors()
+        cmap = ax.collections[0].get_cmap()
+        expected_color = cmap(norm(list(self.df['values'])))
 
-        expected_norm = norm(list(self.df['values']))
-        expected_color = cmap(expected_norm)
-        np.testing.assert_array_equal(actual_norm, expected_norm)
+        # obtain min, max and actual colors from axis of the subset
+        ax1 = self.df[lvl1].plot(column='values', legend=True, norm = norm)
+        actual_lim1 = ax1.collections[0].get_clim()
+        actual_color1 = ax1.collections[0].get_facecolors()
+        cmap1 = ax1.collections[0].get_cmap()
+        expected_color1 = cmap1(norm(list(self.df.loc[lvl1]['values'])))
+
+        # Limits must be equal
+        assert actual_lim == actual_lim1
+
+        # Colors of min and max for both the plot are equal(which is not correct)
+        assert not np.array_equal(actual_color[0],actual_color1[0])
+        assert not np.array_equal(actual_color[-1],actual_color1[-1])
+
+        # Colors are equal in actual_color and expected color
+        np.testing.assert_array_equal(actual_color, expected_color)
+        np.testing.assert_array_equal(actual_color1, expected_color1)
+
+        # Colors of the colors are equal
+        colorbar_color = ax.get_figure().axes[1].collections[0].get_facecolor()
+        colorbar_color1 = ax1.get_figure().axes[1].collections[0].get_facecolor()
+        np.testing.assert_array_equal(colorbar_color, colorbar_color1)
